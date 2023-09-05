800417b74251c75186e9cbaf223b73de187f9cd0
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-09-05 20:44:34 +0300

    add componenrts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experiencs from "./components/Experiencs";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Feedbacks from "./components/Feedbacks";
+import Contact from "./components/Contact";
+import StarsCanvas from "./components/canvas/Stars";
 
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-patter bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experiencs />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
