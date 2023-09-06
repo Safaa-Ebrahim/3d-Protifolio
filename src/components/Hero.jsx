7f1a50f6f36c610d64da6a0149d5dc9f407c6163
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-09-06 19:48:39 +0300

    add details to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,42 +14,45 @@ export default function Hero() {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
-        <div style={{zIndex:100}}>
+        <div style={{ zIndex: 100 }}>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Safaa</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2  text-white-100`} >
-            I'm a react frontend developer, {" "}
+          <p className={`${styles.heroSubText} mt-2  text-white-100`}>
+            I'm a react frontend developer, <br className="sm:block hidden" />
+            I have a strong background in
             <br className="sm:block hidden" />
-            I have a strong background in 
-            <br className="sm:block hidden" /> 
-           creating interactive user interfaces 
-           <br className="sm:block hidden" /> 
-           and developing web application. 
+            creating interactive user interfaces
+            <br className="sm:block hidden" />
+            and developing web application.
           </p>
         </div>
       </div>
-        
-        <ComputersCanvas/>
 
-        <div className="absolute xs:bottom-10 
-        bottom-32 w-full flex justify-center items-center">
-          <a href="#about">
-            <div className="w-[35px] h-[64px] rounded-3xl 
+      <ComputersCanvas />
+
+      <div
+        className="absolute xs:bottom-10 
+        bottom-32 w-full flex justify-center items-center"
+      >
+        <a href="#about">
+          <div
+            className="w-[35px] h-[64px] rounded-3xl 
             border-4 border-secondary
-             flex justify-center items-start p-2">
-              <motion.div 
-              animate={{y:[0,24,0]}}
+             flex justify-center items-start p-2"
+          >
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
               transition={{
-                duration:1.5,
-                repeat:Infinity,
-                repeatType:'loop'
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
               }}
               className="w-3 h-3 rounded-full bg-secondary mb-1"
-              />
-             </div>
-          </a>
-        </div>
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 }
