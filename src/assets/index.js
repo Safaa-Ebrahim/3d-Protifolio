5a900c3cf2ecdf4fc1d37c06e936ca88ce7a8b46
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-10-02 21:02:08 +0300

    modify the asssets

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,8 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -15,14 +13,12 @@ import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import bootstrap from "./tech/bootstrap.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import nti from "./company/nti2.png";
+import iti from "./company/iti.png";
+import collage from "./company/collage.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -36,8 +32,7 @@ export {
   web,
   github,
   css,
-  docker,
-  figma,
+  bootstrap,
   git,
   html,
   javascript,
@@ -45,13 +40,10 @@ export {
   nodejs,
   reactjs,
   redux,
-  tailwind,
   typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  nti,
+  iti,
+  collage,
   carrent,
   jobit,
   tripguide,
