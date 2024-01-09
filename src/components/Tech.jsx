f278ad086f7964fd4f152b7d318f7f88f4bbb570
Safaa-Ebrahim <ebrasafa@gmail.com>
2024-01-09 13:30:24 +0200

    add tecnology name when hover on the ballcanvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import {BallCanvas} from "./canvas";
-import { SectionWrapper } from '../hoc';
-import { technologies } from '../constants';
+import React, { useState } from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 function Tech() {
+  const [hoveredBall, setHoveredBall] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredBall(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredBall(null);
+  };
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology,index)=>{
-       return(
-         <div className='w-28 h-28' key={technology.name}> 
-         <BallCanvas icon={technology.icon} />
-         </div>
-         )
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology, index) => {
+        return (
+          <div
+            className="w-28 h-28"
+            key={technology.name}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <BallCanvas icon={technology.icon} />
+            {hoveredBall === index && (
+              <div className="text-gray-700 text-center">{technology.name}</div>
+            )}
+          </div>
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default SectionWrapper(Tech,"");
-
+export default SectionWrapper(Tech, "");
